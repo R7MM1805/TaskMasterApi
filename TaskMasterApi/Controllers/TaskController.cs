a5310f9d85bbef0603d45fa30ab762213b8c541d
Ricardo Metzger <r7metzger@gmail.com>
2025-08-21 23:02:48 -0500

    feat: listar y detalle de tarea

---
+++ TaskMasterApi/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskMasterApi.Models;
+using TaskMasterApi.Services;
+
+namespace TaskMasterApi.Controllers
+{
+    [Controller]
+    [Route("/api/[controller]")]
+    public class TaskController : Controller
+    {
+        [HttpGet("GetTask")]
+        public ActionResult<List<TaskModel>> GetTask() => Ok(TaskDataStore.Current.Tasks);
+        [HttpGet("GetTaskByID")]
+        public ActionResult<TaskModel> GetTaskByID(int id)
+        {
+            TaskModel? task = TaskDataStore.Current.Tasks.FirstOrDefault(x => x.ID == id);
+            return task is null ? NotFound("La tarea no fue encontrada") : Ok(task);
+        }
+    }
+}
